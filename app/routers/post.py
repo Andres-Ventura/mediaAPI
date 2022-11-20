57c18ec47683b2d8076ebddeb458df1dd901a78a
Andres-Ventura <andres.venturase@gmail.com>
2022-11-19 19:52:14 -0800

    Update

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,6 +1,6 @@
 
 from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
-from .. import models, schemas
+from .. import models, schemas, oauth2
 from sqlalchemy.orm import Session
 from ..database import get_db
 from typing import Optional, List
@@ -18,7 +18,7 @@ def get_posts(db: Session = Depends(get_db)): # may change param b/c I could be 
     return posts
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), get_current_user: int = Depends(oauth2.get_current_user)):
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""", (post.title, post.content, post.published))
     # new_post = cursor.fetchone()
     # conn.commit()
